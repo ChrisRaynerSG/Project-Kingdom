fa17396e13d9c4c2129426d7ba375d190884c0d5
Chris Rayner <crayner@spartaglobal.com>
2025-02-09 01:28:08 +0000

    UPDATE: add event to player attributes to show attributes on character sheet

--- Assets/Models/Player/PlayerAttributes.cs
+++ Assets/Models/Player/PlayerAttributes.cs
@@ -1,79 +1,92 @@
 
+using System;
+
 public class PlayerAttributes {
 
-    private int strength;
+    private int strength = 10;
     public int Strength {
         get{
             return strength;
         } 
         set{
         strength = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int dexterity;
+    private int dexterity = 10;
     public int Dexterity {
         get{
             return dexterity;
         } 
         set{
         dexterity = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int intelligence;
+    private int intelligence = 10;
     public int Intelligence {
         get{
             return intelligence;
         } 
         set{
         intelligence = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int charisma;
+    private int charisma = 10;
     public int Charisma {
         get{
             return charisma;
         } 
         set{
         charisma = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int constitution;
+    private int constitution = 10;
     public int Constitution {
         get{
             return constitution;
         } 
         set{
         constitution = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int wisdom;
+    private int wisdom = 10;
     public int Wisdom {
         get{
             return wisdom;
         } 
         set{
         wisdom = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int luck;
+    private int luck = 10;
     public int Luck {
         get{
             return luck;
         } 
         set{
         luck = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    private int perception;
+    private int perception = 10;
     public int Perception {
         get{
             return perception;
         } 
         set{
         perception = value;
+        OnPlayerAttributesChanged?.Invoke(this);
     }}
 
-    public PlayerAttributes(int strength, 
+    public event Action<PlayerAttributes> OnPlayerAttributesChanged;
+
+    public PlayerAttributes(// all arg constructor
+    int strength, 
     int dexterity, 
     int intelligence, 
     int charisma, 
@@ -89,5 +102,8 @@ public class PlayerAttributes {
         this.wisdom = wisdom;
         this.luck = luck;
         this.perception = perception;
+    }
+
+    public PlayerAttributes(){//No arg constructor
     }
 }
